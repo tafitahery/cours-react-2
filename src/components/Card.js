182daef24c3bd276418717c640d8622bef689a58
Tafita Hery <tafitahery@gmail.com>
2022-07-06 20:43:52 +0300

    Initialisation cours react 2/2

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({ country }) => {
+  return (
+    <div>
+      <li className="card">
+        <img src={country.flags.svg} alt={`drapeau ${country.name.common}`} />
+        <div className="infos">
+          <h2>{country.name.common}</h2>
+          <h4>{country.capital}</h4>
+          <p>Pop. {country.population.toLocaleString()}</p>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/pages/About.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla totam vel
+        aspernatur rem vero dolor nobis, illo, veniam architecto a corrupti in
+        ab fuga quo blanditiis rerum eveniet. Suscipit odio perspiciatis nihil
+        accusantium dolores ipsa vero repellat asperiores nobis non ipsum, natus
+        earum doloribus voluptates qui dignissimos deserunt omnis veniam error
+        quas aperiam. Quo cumque vitae saepe, labore officia iste quas delectus
+        quae, quisquam, optio corrupti inventore nulla hic rerum repudiandae
+        enim et corporis numquam quasi minus animi? Ut harum maxime odit
+        deserunt incidunt sapiente earum culpa? Doloribus dolorum ad tempora
+        expedita, velit culpa soluta. Quis voluptatem fuga ea eaque.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+        voluptatem quibusdam sed libero in, magni delectus eaque, soluta
+        voluptatum tenetur labore deleniti laborum quod alias optio eius odit
+        voluptates dignissimos sequi exercitationem laudantium repudiandae,
+        aperiam culpa. Ipsa dolorem sint ipsum magni quas animi enim dignissimos
+        eos, illo molestiae nulla corrupti. Voluptatem fugit voluptas quod hic
+        sit qui consequatur, sint dolores ab accusamus asperiores necessitatibus
+        repellat soluta, excepturi, molestias eligendi vero aliquid assumenda
+        vitae! Placeat hic dolorem laborum aspernatur voluptatem, laudantium
+        veritatis fugit, animi minus quibusdam modi, magni voluptatibus ducimus
+        rem nobis quidem iusto aliquid in. Voluptatum praesentium et sunt harum,
+        provident quasi autem tempore. Placeat asperiores quam deleniti, nisi
+        natus nesciunt quas, dolor ut optio veritatis excepturi beatae
+        aspernatur dolorum, in aperiam quisquam impedit quod ex a aliquam
+        voluptatum rerum earum magnam. Ad asperiores voluptates explicabo eos
+        error magnam iure dicta pariatur. Temporibus recusandae excepturi quia
+        quam tempora minus facilis at maiores quas. Blanditiis eum ducimus minus
+        sunt rem in iure, dolore ipsa sequi non, ullam ipsam enim aperiam
+        veritatis quas? Harum fugiat, eius ipsa, officia aliquam fugit
+        laboriosam consequatur quaerat recusandae est porro tempora praesentium
+        dolore labore reprehenderit! Quos sequi ut harum laboriosam quisquam
+        deserunt voluptatibus consequatur optio aut.
+      </p>
+    </div>
+  );
+};
+
+export default About;
